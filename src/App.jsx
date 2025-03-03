538f162daafc7b18d62404017f9cdf717c87807e
jezi89 <13301753+jezi89@users.noreply.github.com>
2025-03-03 13:03:33 +0100

    country-list met knop gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
 import './App.css';
+import axios from 'axios';
+import {useState} from "react";
 
 function App() {
+    const [countries, setCountries] = useState([]);
+    const [showCountries, setShowCountries] = useState(false);
+
+
+    async function fetchCountryData() {
+        try {
+            if (!countries.length) {
+                const result = await axios.get('https://restcountries.com/v3.1/all?fields=flag,name,population');
+                setCountries(result.data)
+                setShowCountries(true);
+            } else {
+                setShowCountries(!showCountries);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    function setCountryHelper() {
+        return countries.length === 0 ? "Fetch All Countries" :
+            showCountries ? "Hide Country List" : "Set Country List"
+    }
 
     return (
-
         <>
-            Maak je applicatie hier!
+            <header>
+                <button
+                    onClick={fetchCountryData}
+                    className={`country-button ${countries.length !== 0 && showCountries ? "active" : ""}`}> {setCountryHelper()}
+                </button>
+
+            </header>
+            <div className={`main-wrapper ${!showCountries ? "hide" : ""}`}>
+                {countries.length !== 0 && countries.map((country) => (
+                    <div className="countrylist" key={country.name.common}>
+                        {country.name.common}
+                    </div>
+                ))}
+            </div>
         </>
     )
+
 }
 
-export default App
+
+export default App;
+
--- src/app.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-async function fetchCountryData() {
-    const result = await axios.get('https://restcountries.com/v3.1/all?fields=flag,name,population');
-    console.log(result.data);
-}
-
-fetchCountryData()
